44de00677bd5ba248f125aeaf687894a1f48f814
quocdang <dangvanquocmomo@gmail.com>
2026-04-12 00:01:43 +0700

    remove method dup

--- src/main/java/com/group1/shop_runner/repository/ProductVariantRepository.java
+++ src/main/java/com/group1/shop_runner/repository/ProductVariantRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductVariantRepository extends JpaRepository<ProductVariant, Long> {
-    List<ProductVariant> findByProductId(Long productId);
     List<ProductVariant> findByProduct_Id(Long productId);
     Optional<ProductVariant> findFirstByProductIdOrderByIdAsc(Long productId);
     @Query("""
