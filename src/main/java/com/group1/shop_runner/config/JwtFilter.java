7be8c74df8bb22832ca07873ff0d4566fd760ec5
quocdang <dangvanquocmomo@gmail.com>
2026-04-14 18:35:49 +0700

    bo import thua

--- src/main/java/com/group1/shop_runner/config/JwtFilter.java
+++ src/main/java/com/group1/shop_runner/config/JwtFilter.java
@@ -7,10 +7,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,9 +16,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-import java.util.List;
-
-
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
@@ -30,7 +23,6 @@ public class JwtFilter extends OncePerRequestFilter {
     private JwtUtil jwtUtil;
     @Autowired
     private CustomUserDetailsService userDetailsService;
-    private static final Logger log = LoggerFactory.getLogger(JwtFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -51,7 +43,6 @@ public class JwtFilter extends OncePerRequestFilter {
                         .getBody();
 
                 String username = claims.getSubject();
-
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
                 UsernamePasswordAuthenticationToken authentication =
                         new UsernamePasswordAuthenticationToken(
